82d23c026f340af530d2ec6c902cdda63149adc2
FarisJakpau <farisjaafar418@gmail.com>
2023-01-13 15:33:14 +0800

    Add fix API key for network call

--- app/src/main/java/com/faris/newsapp/api/ArticlesApi.kt
+++ app/src/main/java/com/faris/newsapp/api/ArticlesApi.kt
@@ -1,5 +1,6 @@
 package com.faris.newsapp.api
 
+import com.faris.newsapp.BuildConfig.API_KEY
 import com.faris.newsapp.models.Article
 import com.faris.newsapp.models.BaseResponse
 import com.faris.newsapp.models.SearchResponse
@@ -8,16 +9,16 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ArticlesApi {
-    @GET("mostpopular/v2/viewed/7.json?api-key=Td9ImahThDlNibEg6VJMhirBKVG2XxJS")
+    @GET("mostpopular/v2/viewed/7.json?api-key=${API_KEY}")
     suspend fun getMostViewed(): Response<BaseResponse<Article>>
 
-    @GET("mostpopular/v2/shared/7.json?api-key=Td9ImahThDlNibEg6VJMhirBKVG2XxJS")
+    @GET("mostpopular/v2/shared/7.json?api-key=${API_KEY}")
     suspend fun getMostShared(): Response<BaseResponse<Article>>
 
-    @GET("mostpopular/v2/emailed/7.json?api-key=Td9ImahThDlNibEg6VJMhirBKVG2XxJS")
+    @GET("mostpopular/v2/emailed/7.json?api-key=${API_KEY}")
     suspend fun getMostEmailed(): Response<BaseResponse<Article>>
 
-    @GET("search/v2/articlesearch.json?&api-key=Td9ImahThDlNibEg6VJMhirBKVG2XxJS")
+    @GET("search/v2/articlesearch.json?&api-key=${API_KEY}")
     suspend fun searchArticles(
         @Query("q") query: String,
         @Query("page") page: Int
