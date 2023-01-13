ee23d05f557251c97ac4033cbbe26fec0ad14925
FarisJakpau <farisjaafar418@gmail.com>
2023-01-13 16:16:47 +0800

    Add unit test case

--- app/src/test/java/com/faris/newsapp/ArticlesViewModelTest.kt
+++ app/src/test/java/com/faris/newsapp/ArticlesViewModelTest.kt
@@ -1,9 +1,7 @@
 package com.faris.newsapp
 
-import com.faris.newsapp.api.ArticlesApi
 import com.faris.newsapp.models.*
 import com.faris.newsapp.services.ArticlesStore
-import com.faris.newsapp.services.NetworkRequestManager
 import com.faris.newsapp.services.database.ArticlesDao
 import com.faris.newsapp.ui.articles.ArticlesViewModel
 import kotlinx.coroutines.*
@@ -18,23 +16,21 @@ class ArticlesViewModelTest {
 
     lateinit var articlesStore: ArticlesStore
     private var articlesDao: ArticlesDao = mock()
-    private var articlesApi: ArticlesApi = mock()
-    private var networkRequestManager: NetworkRequestManager = mock()
     private lateinit var viewModel: ArticlesViewModel
 
     private val waiter = CountDownLatch(1)
-    private val dispatcher = StandardTestDispatcher()
+    private val dispatcher = Dispatchers.Unconfined
 
     @Before
     fun setup() {
-        articlesStore = ArticlesStore(networkRequestManager, articlesApi)
+        articlesStore = mock()
 
         viewModel = ArticlesViewModel(articlesStore, articlesDao)
         Dispatchers.setMain(dispatcher)
     }
 
     @Test
-    fun `getMostSharedArticleSuccess`() = runBlocking {
+    fun `getMostSharedArticleSuccess`() = runTest {
         val mockResult = arrayListOf(
             Article(
                 id = 1111,
@@ -45,23 +41,45 @@ class ArticlesViewModelTest {
         )
         val testResult = Result.Success(
             BaseResponse(
-            results = mockResult,
-            num_results = 1,
-            response = Article(
-                id = 1111,
-                title = "Testing ",
-                publishedDate = "20/22/2022",
-                articleMenu = PopularMenu.MostShared
-            ),
-            status = "OK"
-        )
+                results = mockResult,
+                num_results = 1,
+                status = "OK",
+                response = Article(
+                    id = 1111,
+                    title = "Testing ",
+                    publishedDate = "20/22/2022",
+                    articleMenu = PopularMenu.MostShared
+                )
+            )
         )
 
         whenever(articlesStore.getMostShared()).thenReturn(testResult)
 
         viewModel.getArticles(PopularMenu.MostShared)
+        Assert.assertEquals(1, viewModel.articleFlow.value.size)
+    }
 
-        Assert.assertEquals(mockResult.size, viewModel.articleFlow.value.size)
+    @Test
+    fun `getMostSharedArticleFail`() = runTest {
+        val mockResult = arrayListOf<Article>()
+        val testResult = Result.Success(
+            BaseResponse(
+                results = mockResult,
+                num_results = 1,
+                status = "OK",
+                response = Article(
+                    id = 1111,
+                    title = "Testing ",
+                    publishedDate = "20/22/2022",
+                    articleMenu = PopularMenu.MostShared
+                )
+            )
+        )
+
+        whenever(articlesStore.getMostShared()).thenReturn(testResult)
+
+        viewModel.getArticles(PopularMenu.MostShared)
+        Assert.assertFalse(viewModel.articleFlow.value.isNotEmpty())
     }
 
     @After
